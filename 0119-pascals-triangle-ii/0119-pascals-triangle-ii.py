ec933abde4320d5979ca65c8e070393548cbe510
zombie035 <167063563+zombie035@users.noreply.github.com>
2026-08-08 11:13:36 +0530

    0119-PASCALS-TRIANGLE-II Stats: Time: 0 ms (100%), Space: 19.5 MB (22.2%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,9 +1,19 @@
-from math import comb
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         l = []
-        
         for i in range(rowIndex+1):
             x = comb(rowIndex,i)
             l.append(x)
         return l
+    def factorial(n):
+        if n < 0:
+            raise ValueError("Factorial is not defined for negative numbers")
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+    def comb(x, y):
+        if y < 0 or y > x:
+            return 0  # or raise error
+        return factorial(x) // (factorial(y) * factorial(x - y))
